4f6280440844fc2e0d64de7bc6eeace8be2f98b9
sun4395 <sun4395@naver.com>
2021-08-29 22:05:05 +0900

    homework8 - add error handling project - add user

--- solo-play/ms/homework8/back/server.js
+++ solo-play/ms/homework8/back/server.js
@@ -4,42 +4,53 @@ const fs = require(`fs`);
 const hostname = `127.0.0.1`;
 
 const mimeType = {
-    ".ico": "image/x-icon",
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".png": "image/png",
-    ".jpg": "image/jpeg",
-    ".eot": "aplication/vnd.ms-fontobject",
-    ".ttf": "aplication/font-sfnt",
-  }
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.eot': 'aplication/vnd.ms-fontobject',
+  '.ttf': 'aplication/font-sfnt',
+};
 
 function start(port) {
-    function onRequest(request, response) {
-        const { headers, method, url } = request;
+  function onError(response) {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write(`Error 404: Resource not found`);
+    response.end();
+  }
 
-        switch(url) {
-            case `/`: 
-                response.writeHead(200, {'Content-Type': mimeType[".html"]});
-                fs.createReadStream(`./front/index.html`).pipe(response);
-                break;
-            case `/style.css`:
-                response.writeHead(200, {'Content-Type':mimeType[".css"]});
-                fs.createReadStream(`./front/style.css`).pipe(response);
-                break;
-            case `/member.js`:
-                response.writeHead(200, {'Content-Type':mimeType[".js"]});
-                fs.createReadStream(`./front/member.js`).pipe(response);
-                break;
-            case `/fonts/NanumBarunpenR.ttf`:
-                response.writeHead(200, {'Content-Type': mimeType[".ttf"]});
-                fs.createReadStream(`./fonts/NanumBarunpenR.ttf`).pipe(response);
-                break;
-        }
+  function onRequest(request, response) {
+    const { headers, method, url } = request;
 
+    switch (url) {
+      case `/`:
+        response.writeHead(200, { 'Content-Type': mimeType['.html'] });
+        fs.createReadStream(`./front/index.html`).pipe(response);
+        break;
+        fs.create;
+      case `/style.css`:
+        response.writeHead(200, { 'Content-Type': mimeType['.css'] });
+        fs.createReadStream(`./front/style.css`).pipe(response);
+        break;
+      case `/member.js`:
+        response.writeHead(200, { 'Content-Type': mimeType['.js'] });
+        fs.createReadStream(`./front/member.js`).pipe(response);
+        break;
+      case `/fonts/NanumBarunpenR.ttf`:
+        response.writeHead(200, { 'Content-Type': mimeType['.ttf'] });
+        fs.createReadStream(`./fonts/NanumBarunpenR.ttf`).pipe(response);
+        break;
+      default:
+        onError(response);
     }
-    const server = http.createServer(onRequest).listen(port, hostname);
-    
+  }
+  const server = http.createServer(onRequest).listen(port, hostname);
+
+  server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
 }
 
 exports.start = start;
